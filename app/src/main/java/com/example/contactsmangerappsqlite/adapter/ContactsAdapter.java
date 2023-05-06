497d8f92121e07e630f5f86b0a1bf9177d64f18c
gulnara_azizova <gazizova12047@ada.edu.az>
2023-05-06 13:02:42 +0400

    Added menu and adapter class

---
+++ app/src/main/java/com/example/contactsmangerappsqlite/adapter/ContactsAdapter.java
@@ -0,0 +1,80 @@
+package com.example.contactsmangerappsqlite.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactsmangerappsqlite.MainActivity;
+import com.example.contactsmangerappsqlite.R;
+import com.example.contactsmangerappsqlite.db.entity.Contact;
+
+
+import java.util.ArrayList;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+
+    private Context context;
+    private ArrayList<Contact> contactsList;
+    private MainActivity mainActivity;
+
+
+    //ViewHolder
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView name;
+        public TextView email;
+        public TextView phoneNumb;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            this.name = itemView.findViewById(R.id.name);
+            this.email = itemView.findViewById(R.id.email);
+            this.phoneNumb = itemView.findViewById(R.id.phoneNumb);
+        }
+    }
+
+
+    //Contacts Adapter
+    public ContactsAdapter(Context context,ArrayList<Contact> contacts,MainActivity mainActivity){
+        this.context = context;
+        this.contactsList  = contacts;
+        this.mainActivity = mainActivity;
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.contact_list_item,parent,false);
+
+        return new MyViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int positions) {
+        final Contact contact = contactsList.get(positions);
+
+        holder.name.setText(contact.getName());
+        holder.email.setText(contact.getEmail());
+        holder.phoneNumb.setText(contact.getPhoneNumb());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.addAndEditContacts(true,contact,positions);
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount(){
+        return contactsList.size();
+    }
+}
