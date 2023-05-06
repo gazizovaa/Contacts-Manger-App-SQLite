6f1130317492c60e401d2b7acc1a93ce4d54c42f
gulnara_azizova <gazizova12047@ada.edu.az>
2023-05-06 18:03:30 +0400

    Updated Main activity

--- app/src/main/java/com/example/contactsmangerappsqlite/MainActivity.java
+++ app/src/main/java/com/example/contactsmangerappsqlite/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.contactsmangerappsqlite.db.entity.Contact;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addAndEditContacts(boolean b, Contact contact, int positions) {
     }
 }
